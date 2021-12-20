2d161583956624c5a8fd01c7eb40c8a26f6d9b3e
austin espinal <austinespi@gmail.com>
2021-12-19 19:37:33 -0500

    add overall project template. still working on details

---
+++ index.js
@@ -0,0 +1,3 @@
+const {writeFile, copyFile} = require('./src/generateHTML');
+const inquirer = require('inquirer');
+const template = require('./src/pageTemplate');
---
+++ src/generateHTML.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject)=> {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'file created!'
+            });
+        });
+    });
+};
+const copyFile = () => {
+    return new Promise((resolve, reject)=> {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'style sheet created!'
+            });
+        });
+    });
+};
+
+module.exports = {writeFile, copyFile};
